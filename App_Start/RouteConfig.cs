bb26f564b2c8291cfed5b760df974cb521da3d83
pwint <ppkyaw@gmail.com>
2018-06-06 21:04:10 +0700

    publish to azure

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,10 +13,17 @@ namespace MVCDemoApp
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "MVCDemoApp.Controllers" }
             );
         }
     }
